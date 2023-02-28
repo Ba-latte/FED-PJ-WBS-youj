c10a03bb4785ba109fe6df3c56dc7e611230aaeb
Ba-latte <youj_lim@naver.com>
2023-02-28 17:18:44 +0900

    2/28

--- 04.팀 프로젝트/03.구현소스/js/sub_earth.js
+++ 04.팀 프로젝트/03.구현소스/js/sub_earth.js
@@ -73,17 +73,44 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
         for(let x of figure) expansionFn(x);
 
-
-
-
-
-
-
-
-
     }); /////////////////////// scroll 이벤트 끝 ///////////////////////
 
 
+
+
+
+    // 2페이지 박스에 마우스가 올라가서 움직이면 마우스 따라다니는 동그라미 만들기
+    // 1.대상선정
+    // 1-1.움직일 대상 : .move-mouse-pointer
+    const move_mouse_pointer = document.querySelector(".move-mouse-pointer");
+    // 1-2.마우스가 올라갔을 때의 대상 : .pg2
+    const pg2 = document.querySelector(".pg2");
+    // 1-3.동그라미와 마우스포인터 간 갭 조정
+    let gap = move_mouse_pointer.clientWidth / 2;
+
+
+    console.log("대상 선정: ", move_mouse_pointer);
+
+
+
+    // 2.이벤트 적용 : mousemove 이벤트
+    pg2.onmousemove = () => {
+        event.stopPropagation();
+
+        // 마우스 포인터 위치 알아내기
+        // console.log("x: ", event.clientX, "\n y: ", event.clientY);
+
+        // 위치값 보정하기 (gap은 move_mouse_pointer 크기의 절반임!)
+        let posX = event.offsetX - gap;
+        let posY = event.offsetY - gap;
+
+        // move_mouse_pointer에게 위치값에 따라 이동하도록 세팅하기 (move_mouse_pointer의 중앙위치 보정)
+        move_mouse_pointer.style.top = posY + "px";
+        move_mouse_pointer.style.left = posX + "px";
+    }; //////////////////// mousemove 이벤트 함수 끝 /////////////////////
+
+    // pg2영역 바깥으로 나가면 사라지고 들어오면 나타나게 하기
+    pg2.onmouseenter = ()=>{move_mouse_pointer.style.display = "block";};
 
 
 
