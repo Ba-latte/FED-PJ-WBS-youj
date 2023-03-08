ef61a891be057696cb38280e9b7136274a092a51
Ba-latte <youj_lim@naver.com>
2023-03-08 17:18:40 +0900

    3/8

---
+++ 01.웹프로젝트/03.구현소스/js/video_data.js
@@ -0,0 +1,15 @@
+/* 동영상 데이터 리스트 - video_data.js */
+
+const videoObj = [
+    {tit:"2022 서울미식주간 소개",vID:"317Ctu99DF8"},
+    {tit:"그랜드 갈라 디너 행사 스케치",vID:"V0rs1nphDuQ"},
+    {tit:"서울 마르쉐 행사 스케치",vID:"iIr24qhBPHE"},
+    {tit:"공로상 수상자 소개",vID:"h1fWv-juTjQ"},
+    {tit:"바&펍 부문 수상자 소개",vID:"UbIxKax1SSc"},
+    {tit:"카페&디저트 부문 수상자 소개",vID:"ZniSjaTurJU"},
+    {tit:"채식부문 수상자 소개",vID:"49sIILYBGWE"},
+    {tit:"그릴 부문 수상자 소개",vID:"XG5D_mERRS8"},
+    {tit:"아시안 부문 수상자 소개",vID:"oHy9vcxM4cA"},
+    {tit:"양식 부문 수상자 소개",vID:"9frWal_xAMA"},
+    {tit:"한식 부문 수상자 소개",vID:"VUS99CniZ5c"},
+];
