8747ef6bafcb1bceaed63bcc8459fc1478ebeaf7
Bara <youj_lim@naver.com>
2023-05-05 19:54:34 +0900

    5/5

---
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/linksys.js
@@ -0,0 +1,99 @@
+// 불가리 PJ 링크 시스템 JS - linksys.js //////
+// : 메뉴 클릭하면 거기로 가게 만들기
+
+
+///////////////////////////// [ 제이쿼리 로드 구역 ] /////////////////////////////
+$(()=>{
+    console.log("링크시스템 JS - 로딩 완료");
+    /***************************************************
+        sns 메뉴 파트 링크 세팅하기 (DT + Mobile)
+        !!주의!! 항상 html DOM 변경 후에 이벤트 작업을 할 것!!
+        먼저 이벤트를 걸고 DOM 변경을 하면 이벤트가 풀린다~!
+    ***************************************************/
+    $(".tab_bx>.tit_tab>div").click(function(e){
+        // 기본 이동 막기
+        e.preventDefault();
+
+        // 1.클릭된 a요소 text 읽기 + 앞뒤공백제거(trim())
+        let atxt = $(this).find("a").text().trim();
+        console.log(atxt);
+
+        // 2. 클릭된 탭에 따라서 박스 골라 보여주기
+        if(atxt === "로그인"){
+            console.log("로그인 탭 클릭했음");
+            // 로그인 탭의 아랫경계선 진하게 표시하기 + 형제 탭은 지우기
+            $(this).css({borderBottom: "3px solid var(--black)"})
+            .siblings().css({borderBottom: "none"});
+
+            // 로그인 탭 박스 보이기 + 형제 탭은 숨기기
+            $("section.login").show()
+            .siblings("section.member, section#terms").hide();
+        }
+        else if(atxt === "회원가입"){
+            console.log("회원가입 탭 클릭했음");
+            // 회원가입 탭의 아랫경계선 진하게 표시하기 + 형제 탭은 지우기
+            $(this).css({borderBottom: "3px solid var(--black)"})
+            .siblings().css({borderBottom: "none"});
+
+            // 회원가입 탭 박스 보이기 + 형제 탭은 숨기기
+            $("section#terms").show()
+            .siblings("section.member, section.login").hide();
+        }
+
+
+    }); ////////////////// click ///////////////////
+
+
+
+}); ////////////////////////////////////// jQB ///////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////// 로딩 구역 ////////////////////////////////////////
+// window.addEventListener("DOMContentLoaded", linkFn);
+
+////////////////////////// 링크시스템 로드 함수 //////////////////////////////
+function linkFn(){
+
+    console.log("링크 로딩완료!");
+
+    // 1.링크 대상 선정
+    // (1) GNB : .gnb a
+    const gnb = document.querySelectorAll(".gnb a, .mognb a");
+    // console.log(gnb);
+    // (2) 로고 : .logo a
+    const logo = document.querySelector(".logo a");
+
+    
+    // 2.클릭 이벤트 설정하기
+    // (1) GNB 클릭 설정
+    for(let x of gnb){
+        x.onclick = (e)=>{
+            // (0) 클릭 이동기능 막기
+            e.preventDefault();
+
+            // (1) 클릭된 a요소의 텍스트를 읽어오기 + 소문자로 텍스트 바꾸기
+            let atxt = x.innerText.toLowerCase().trim();
+            // toLowerCase() : 소문자로 바꾸기
+            // toUpperCase() : 대문자로 바꾸기
+            // trim() : 앞/뒤 공백 제거하기
+
+            console.log(atxt);
+
+            // (2) 서브 페이지 이동하기 : "search"가 아니면 서브페이지로 이동하라는 뜻!
+            if(atxt !== "search") location.href = "category.html?cat=" + encodeURIComponent(atxt);
+            // encodeURIComponent() : 2byte문자나 특수문자가 있을 경우, 인코딩해줘야함! -> 받아가는 곳에서도 디코딩 해줘야 정확히 나옴
+
+        }; /////////////////// onclick 이벤트 끝 ///////////////////
+    } //////////////////// for of문 끝 ////////////////////////
+
+
+
+} //////////////////////// linkFn 함수 끝 ///////////////////////
