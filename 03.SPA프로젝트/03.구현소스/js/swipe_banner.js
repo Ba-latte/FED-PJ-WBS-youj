9bc2be1e6b823b3061d4bdd00daeac05ba524f41
Ba-latte <youj_lim@naver.com>
2023-05-31 16:18:34 +0900

    5/31

--- 03.SPA프로젝트/03.구현소스/js/swipe_banner.js
+++ 03.SPA프로젝트/03.구현소스/js/swipe_banner.js
@@ -6,12 +6,20 @@ $(()=>{
     let swipe_banner = new Swiper(".product_swipe", {
         slidesPerView: 4,
         spaceBetween: 30,
-        centeredSlides: true,
+        centeredSlides: true, // 센터모드 : true인 경우 활성 슬라이드가 항상 왼쪽이 아니라 중앙에 배치됨
+        // centeredSlidesBounds: true, // 센터모드 true인 경우에만 활성화 가능함. 슬라이드 가운데에 올 때 오른쪽에 빈 공간 두지 않음
+        // loop: true, // 센터 모드일때엔 안 쓰는게 좋음
+        // loopAdditionalSlides: 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+        // grabCursor: true, // dt일때 마우스포인터 설정
         pagination: {
             el: ".swiper-pagination",
+            type: "progressbar",
             clickable: true,
         },
     });
     
     swipe_banner;
+
+    // AOS 라이브러리
+    AOS.init();
 });
