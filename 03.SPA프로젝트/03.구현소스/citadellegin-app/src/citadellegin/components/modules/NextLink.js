9ae9eddd3d10b1f3878f98d3e08153015b86c4bc
Ba-latte <youj_lim@naver.com>
2023-06-28 12:45:53 +0900

    6/28

--- 03.SPA프로젝트/03.구현소스/citadellegin-app/src/citadellegin/components/modules/NextLink.js
+++ 03.SPA프로젝트/03.구현소스/citadellegin-app/src/citadellegin/components/modules/NextLink.js
@@ -16,13 +16,18 @@ const NextLink = (props)=>{
 
     // 들어온 페이지의 다음 페이지 데이터 넣기
     const next = (pgname)=>{
+        // 객체 배열에서 속성들만 뽑아서 배열로 만들기
         let key = Object.keys(selcData);
+        // 객체 배열에서 값들만 뽑아서 배열로 만들기
         let val = Object.values(selcData);
+        // 속성만 뽑은 배열에서 파라미터로 받아온 페이지 이름(현재 페이지)속성의 순번에다 +1을 하면 다음 페이지의 이름(속성)의 순번이 됨
         let num = key.indexOf(pgname) + 1;
         // console.log("다음 객체 소환!", key[num].toLowerCase());
         
+        // 제일 마지막 순번인 3번을 지나서 4번이 되면 0으로 되돌려서 다시 처음 페이지 이름 속성의 순번을 할당하기
         if(num == 4) num = 0;
         
+        // 원본 객체 배열에서 값만 뽑은 val배열에서 데이터 뽑아서 리턴하기
         return [val[num].pdtit[0], val[num].pdtit[1], key[num]];
     }
 
